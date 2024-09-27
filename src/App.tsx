84bc2529124c5827002751ea66f88b91ab0a9eb5
matthewalton <mattalton97@gmail.com>
2024-09-27 17:50:51 +0100

    move into sequencer directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css"
-import SequencerInterface from "./components/sequencer-interface"
+import SequencerInterface from "./components/sequencer/sequencer-interface"
 
 export default function App() {
   return (
