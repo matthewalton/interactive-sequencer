fdc94f59b50d8af882c68eec928d8e9f4e93b30c
matthewalton <mattalton97@gmail.com>
2024-09-27 16:54:57 +0100

    setup component with tailwind classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,10 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
 import "./App.css"
-import { Button } from "./components/ui/button"
+import Sequencer from "./components/sequencer"
 
 export default function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="flex flex-col items-center justify-center gap-5 h-screen">
-      <div className="flex flex-row items-center justify-center gap-4">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <h1 className="text-3xl font-bold underline">Vite + React</h1>
-
-      <div className="flex flex-col items-center justify-center gap-4">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+    <div className="flex h-screen flex-col items-center justify-center gap-5">
+      <Sequencer />
     </div>
   )
 }
---
+++ src/components/sequencer.tsx
@@ -0,0 +1,7 @@
+export default function Sequencer() {
+  return (
+    <div className="h-4/6 w-10/12 rounded-sm border p-4 text-center shadow">
+      Sequencer
+    </div>
+  )
+}
